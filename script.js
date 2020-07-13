1c345ecf4c4bd90c553d0394a937bfede8627bbd
arantiasunardi <67307355+arantiasunardi@users.noreply.github.com>
2020-07-13 16:10:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const calculatorScreen = document.querySelectorAll('.calculator-screen')
+
+const updateScreen = (number)=>{
+    calculatorScreen.value=number
+}
+
+const numbers= document.querySelectorAll(".number")
+
+
+numbers.forEach( (number)=>{
+    number.addEventListener("click", (event)=> {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber) 
+    })
+})
+
+const calculatorScreen = document.querySelectorAll('.calculator-screen')
+
+const updateScreen = (number)=>{
+    calculatorScreen.value=number
+}
+
+let prevNumber=''
+let calculatorOperator=''
+let currentNumber='0'
+
+const inputNumber= (number)=>{
+    if (currentNumber === '0') {
+        currentNumber = number
+    } else {
+    currentNumber += number
+    }
+}   
+
+const operators = document.querySelectorAll(".operator")
+
+operators.forEach((operator)=>{
+    operator.addEventListener("click", (event)=>{
+        inputOperator(event.target.value)
+    })
+})
+
+const inputOperator = (operator) => {
+    prevNumber = currentNumber
+    calculationOperator = operator
+    currentNumber = ''
+}
+
+const equalSign = document.querySelector('.equal-sign')
+
+equalSign.addEventListener('click', () => {
+    console.log('equal button is pressed')
+})
